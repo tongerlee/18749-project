e73fdb74683b4f0512c4f4cce8b4c51c9829ad73
zerir <zerir@zerirdeMBP-2.wv.cc.cmu.edu>
2019-11-04 15:58:37 -0500

    original
    1

---
+++ TCP stuff/lfd_1.py
@@ -0,0 +1,15 @@
+import socket
+import sys
+import time
+from multiprocessing import Process
+
+#while true:
+#	time.sleep(2)
+
+	#if : #get messsage 
+		# send message to gfd
+p = Process(target = f ,args('bob',))
+p.start()
+p.join()
+
+    
