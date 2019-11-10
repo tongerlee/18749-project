deb78d252cdeddd19e879201377c62a657172ba5
Ruiping Luo <ruipingl@andrew.cmu.edu>
2019-11-10 13:01:35 -0500

    Revert "changed gfd lfd"
    This reverts commit 67f4339040075e3da8fc97dd04e70388b45d69ec.

--- localfaultdetector/lfd.py
+++ localfaultdetector/lfd.py
@@ -2,13 +2,15 @@ import socket
 import sys
 import time
 from multiprocessing import Process
+import tcp_server
 import tcp_client 
-
+import _thread
+my_ip_address = ('localhost', 8888)
 heartbeat_message = "alive"
 server_ip_address =  ('localhost', 8080)
 alive_message = "Server is alive."
 dead_message = "Server is dead."
-gfd_ip_address =  ('localhost', 8000)
+gfd_ip_address =  ('128.237.198.254', 8000)
 
 def heartbeat():
 	while True:
@@ -16,8 +18,7 @@ def heartbeat():
 		try:
 			tcp_client.send_to(server_ip_address ,heartbeat_message,1)# wait 2 sec
 		except:
-			tcp_client.flag = 0# assume failed
-		#print (tcp_client.flag, tcp_client.msg)
+			pass
 		if tcp_client.flag == 1 and tcp_client.msg == heartbeat_message: #get messsage 
 			print(alive_message)# send message to gfd
 			try:
@@ -29,7 +30,9 @@ def heartbeat():
 			print(dead_message)
 			try:
 				tcp_client.send_to(gfd_ip_address,dead_message,0)
+				print ('msg sent')
 			except:
 				pass
 
-heartbeat()
+_thread.start_new_thread(heartbeat(),tuple("thread_1"))
+_thread.start_new_thread(tcp_server.listen_thread(my_ip_addressip_address,0),tuple("thread_2"))
--- localfaultdetector/tcp_client.py
+++ localfaultdetector/tcp_client.py
@@ -1,38 +1,38 @@
 import socket
 import sys
 import time
-from threading import Thread
 
-timeoutflag = 0
 
-def timeout(sleeptime):
-    time.sleep(sleeptime)
-    timeoutflag =1
-
-def send_to(IP , message, waittime):
+def send_to(IP , message, wait):
     global msg
     global flag
     msg = ''
     flag = 0
     # Create a TCP/IP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Connect the socket to the port where the server is listening
+#    server_address = ('localhost', 10000)
+    #print ( 'connecting to ', IP)
     sock.connect(IP)
 
     try:
         sock.sendall(message.encode())
         # Look for the response
         amount_received = 0
-        amount_expected = len(message)
-        if waittime >0:
-            timeoutflag =0
-            Thread(target=timeout, args=[waittime]).start()
-        else:
-            timeoutflag=1
-        while amount_received < amount_expected and timeoutflag ==0:
-            data = sock.recv(1024)
+        amount_expected = len(message) 
+        count = 0
+        while amount_received < amount_expected and count<2 and wait ==1:
+            print (count)
+            count+=1
+            time.sleep(1)
+            data = sock.recv(16)
             if data:
                 flag = 1
                 msg += data.decode("utf-8") 
             amount_received += len(data)
+            #print ('received ', data)
+
     finally:
+        #print ('closing socket')
         sock.close()
