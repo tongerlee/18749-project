19f652c0e87f34243a1b7f9efa0b5ac7daf87c9c
yichenl2 <yichenl2@andrew.cmu.edu>
2019-12-06 21:23:35 -0500

    more comment for debug

--- client/client.py
+++ client/client.py
@@ -5,7 +5,7 @@ import time
 from threading import Thread
 
 client_id = sys.argv[1]
-rm_IP = '128.237.119.74'
+rm_IP = '128.237.211.64'
 rm_port = 10001
 server_port = 8080
 
@@ -38,16 +38,17 @@ def work():
             primary_ip = server_list[0][0]
             back_up_ips = server_list[1:]
             try:
-                server_address = (primary_ip, server_port)
-                # print('connecting to server %s port %s' % server_address)
-                sock.connect(server_address)
-
+                primary_server_address = (primary_ip, server_port)
+                print('connecting to [primary] %s' % primary_ip)
+                sock.connect(primary_server_address)
                 sock.sendall(str.encode(inputData))
                 receiveData = sock.recv(1024).decode("utf-8")
                 response = receiveData
+
                 # print received server response
                 print(primary_ip, ",", time.ctime(), ",", response)
                 print('[response] : %s' % response)
+
             except:
                 while server_list[0][0] == primary_ip:
                     continue
@@ -58,9 +59,9 @@ def work():
         for ip in back_up_ips:
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             try:
-                server_address = (ip[0], server_port)
-                # print('connecting to server %s port %s' % server_address)
-                sock.connect(server_address)
+                backup_server_address = (ip[0], server_port)
+                print('connecting to [backup] %s' % ip[0])
+                sock.connect(backup_server_address)
                 sock.sendall(str.encode(inputData))
 
             except:
@@ -75,7 +76,7 @@ def connect_replicate_manager():
     global rm_port
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_address = (rm_IP, rm_port)
-    print('connecting to rm at %s port %s ...' % server_address)
+    print('connecting to rm %s port %s ...' % rm_IP)
     sock.connect(server_address)
     try:
 
